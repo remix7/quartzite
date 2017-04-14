7542e96cc547d109eaf7006aaba29e3753d2361a
liutao <echoesgml@gmail.com>
2017-04-14 19:55:04 +0800

    添加客户端

---
+++ src/main/java/cn/echoes/quartzite/client/Client.java
@@ -0,0 +1,10 @@
+package cn.echoes.quartzite.client;
+
+/**
+ * -------------------------------------
+ * TODO
+ * -------------------------------------
+ * Created by liutao on 2017/4/14 下午7:52.
+ */
+public class Client {
+}
