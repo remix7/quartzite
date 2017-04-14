20597699d2a1f2354128b143da9c405a4652c09c
liutao <echoesgml@gmail.com>
2017-04-14 19:54:51 +0800

    添加cron生成器

---
+++ src/main/java/cn/echoes/quartzite/utils/CronUtil.java
@@ -0,0 +1,10 @@
+package cn.echoes.quartzite.utils;
+
+/**
+ * -------------------------------------
+ * quartz cron 根据参数转换规则
+ * -------------------------------------
+ * Created by liutao on 2017/4/14 下午7:51.
+ */
+public class CronUtil {
+}
