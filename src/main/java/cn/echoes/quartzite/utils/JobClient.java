b9103a1839cc3c0bb29d2adb6e14079e303ab130
liutao <echoesgml@gmail.com>
2017-04-20 16:05:16 +0800

    删除测试文件

---
+++ src/main/java/cn/echoes/quartzite/utils/JobClient.java
@@ -0,0 +1,18 @@
+package cn.echoes.quartzite.utils;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+/**
+ * -------------------------------------
+ * TODO
+ * -------------------------------------
+ * Created by liutao on 2017/4/20 下午3:56.
+ */
+public class JobClient implements Job {
+    @Override
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+
+    }
+}
